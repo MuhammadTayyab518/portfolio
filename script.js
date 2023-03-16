0999dd7222c7704c6b75fadb1192aa495af1393e
MuhammadTayyab518 <125296175+MuhammadTayyab518@users.noreply.github.com>
2023-03-16 10:40:44 +0500

    upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".multiple-text", {
+    strings: ["Frontend Developer.", "YouTuber.", "Blogger."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backdelay: 1000,
+    loop: true
+})
